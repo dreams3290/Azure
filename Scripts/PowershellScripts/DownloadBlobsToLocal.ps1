94dc5b6b33cc73e73f0ad2ae54be91c53963f6eb
Sanjay <sramadugu1@gmail.com>
2018-03-08 09:36:48 -0600

    Script to download all the files in the container to local

---
+++ Scripts/PowershellScripts/DownloadBlobsToLocal.ps1
@@ -0,0 +1,15 @@
+﻿#This script is to copy all the files in the container to local
+
+
+$containerName='containerName'
+$destinationPath='destinationPath'
+$connectionString='connectionString'
+$storageAccount= New-AzureStorageContext -ConnectionString $connectionString
+$blobs= Get-AzureStorageBlob -Container $containerName -Context $storageAccount
+foreach ($blob in $blobs)
+{ 
+New-Item -ItemType Directory -Force -Path $destinationPath
+ 
+ Get-AzureStorageBlobContent -Container $containerName -Blob $blob.Name -Destination $destinationPath -Context $storageAccount
+
+}
