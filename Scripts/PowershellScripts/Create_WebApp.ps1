2465fb6960a9ee92bcd755205eedc4adf71b1009
Sanjay <sramadugu1@gmail.com>
2018-03-15 15:57:42 -0600

    script modified

--- Scripts/PowershellScripts/Create_WebApp.ps1
+++ Scripts/PowershellScripts/Create_WebApp.ps1
@@ -1,35 +1,14 @@
-﻿Login-AzureRmAccount 
+﻿#parameters
+$resourceGroupName='testRG'
+$resourceGroupLocation='North Central US'
+$appservicePlanName='testAppServicePlan'
+$webAppName='testwebapp320'
+#create a new Resource group
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force
+#create a new Resource plan
+$appServicePlan = New-AzureRmAppServicePlan -Name $appservicePlanName -ResourceGroupName $resourceGroupName -Location $resourceGroupLocation -Tier Standard -WorkerSize Small
+#create a new Web app
+New-AzureRmWebApp -ResourceGroupName $resourceGroupName  -Location $resourceGroupLocation -AppServicePlan $appServicePlan.ServerFarmWithRichSkuName -Name $webAppName 
 
-#Define properties for the app service plan
-$resourceGroupName="AZ533RGtest"
-$appServicePlanName="az533plan"
-$location="West US"
-$tier="Premium"
-$workerSize="small"
 
-# Create a new resource group
-
-#New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
-
-#create a new app service plan
-#New-AzureRmAppServicePlan -ResourceGroupName $resourceGroupName -Name $appServicePlanName -Location $location -Tier $tier -WorkerSize $workerSize
-
-#create a new web app
-$webAppName="testwebapp3299"
-#New-AzureRmWebApp -ResourceGroupName $resourceGroupName -Location $location -AppServicePlan $appServicePlanName -Name $webAppName
-
-#create a new weba pp deployment slot
-$stagingSlotName="Staging"
-New-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $webAppName -Slot $stagingSlotName
-
-#Get Reference to Production Slot
-$productionSlotName="Production"
-$productionSite= Get-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $webAppName -Slot $productionSlotName
-
-#create a staging that clones production slot
-New-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $webAppName -Slot $stagingSlotName -AppServicePlan $appServicePlanName -SourceWebApp $productionSite
-
-#swap staging and production deploymetn slots
-
-Swap-AzureRmWebAppSlot -ResourceGroupName $resourceGroupName -Name $webAppName -SourceSlotName $stagingSlotName -DestinationSlotName $productionSlotName
 
