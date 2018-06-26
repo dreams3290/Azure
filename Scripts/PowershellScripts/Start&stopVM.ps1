f49db5bfb6afb929ce822e92ccb9902c13297a0f
dreams3290 <sramadugu1@gmail.com>
2018-06-26 16:12:50 -0500

    Script corrected

--- Scripts/PowershellScripts/Start&stopVM.ps1
+++ Scripts/PowershellScripts/Start&stopVM.ps1
@@ -1,9 +1,10 @@
 Add-AzureRmAccount
 $subscriptionID="Enter your subscription ID"
 $subscription=Get-AzureRmSubscription -SubscriptionId $subscriptionID
+Select-AzureRmSubscription -Subscription $subscription
 $resourceGroupName="Enter your resource Group name"
 $vmName="Enter the name of your Vm"
 #Start the VM
 Start-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
-#Stop teh VM
+#Stop the VM
 Stop-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
